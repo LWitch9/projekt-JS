89b3d99cc5cb6a15a563444d12f686f7ae1546c1
Agness <agix999r@gmail.com>
2020-05-11 21:15:43 +0200

    In UserInterface. Faza_rozmieszczanie provisionally made

--- Ships/Functionality.py
+++ Ships/Functionality.py
@@ -3,9 +3,11 @@ class Ship():
 
     def __init__(self, x1, y1, x2, y2):
         self.__list_of_coordinates = [(x1, y1),(x2,y2)]
-    # konstruktor dla jednomasztowca
+
+    #TODO po odkomentowaniu konstruktora dla jednomaszt nie działa konstruktor wyzej dlaczego?
+    #konstruktor dla jednomasztowca
     #def __init__(self, x1, y1):
-     #   self.__list_of_coordinates=[(x1,y1)]
+    #    self.__list_of_coordinates=[(x1,y1)]
 
     def take_list(self):
         return self.__list_of_coordinates
@@ -41,14 +43,14 @@ class ShipsContainer():
         self.__list_of_ships=[]
         self.__ships_to_set=[4,3,3,2,2,2,1,1,1,1]
 
-    def add_ship(self):
+    def add_ship(self,x,y):
         if not self.__ships_to_set:
             print("Ustawiles juz wszystkie statki! Przejdz do gry")
             return
-        print("Podaj wspolrzedne statku: ")
-        x = int(input("Podaj x: "))     #Wspolrzedne jednego konca sktaku
-        y = int(input("Podaj y: "))
 
+        if(x<1 or x>10 or y<1 or y>10):
+            print("Plansza jes wymiarow 10 x 10! Podane wpolrzedne nie mieszcza sie w planszy")
+            return
         if self.__ships_to_set.count(1):
             #Akcje sprawdzające czy dany statek moze zostac umieszczony
             for i in self.__list_of_ships:
@@ -65,21 +67,19 @@ class ShipsContainer():
         else:
             print("Ustawiles juz wszystkie jednomasztowce!")
 
-    def add_ship2(self):
+    def add_ship2(self,x,y,x2,y2):
         if not self.__ships_to_set:
             print("Ustawiles juz wszystkie statki! Przejdz do gry")
             return
-        print("Podaj wspolrzedne statku: ")
-        x = int(input("Podaj x: "))     #Wspolrzedne jednego konca sktaku
-        y = int(input("Podaj y: "))
 
-        print("Podaj wspolrzedne statku: ")
-        x2 = int(input("Podaj x: "))     #Wspolrzedne drugiego konca sktaku
-        y2 = int(input("Podaj y: "))
-
+        if(x<1 or x>10 or x2<1 or x2>10 or y<1 or y>10 or y2<1 or y2>10):
+            print("Plansza jes wymiarow 10 x 10! Podane wpolrzedne nie mieszcza sie w planszy")
+            return
         if x2 != x and y2 != y:  # Sprawdzenie pion/poziom po tym czy ktores wspolrzedne sa takie same
             print("Statek moze byc ustawiony tylko w poziomie lub pionie")
+            return
         else:
+            dl=0
             if x2 != x:
                 dl = abs(x2 - x)+1  # dlugosc zadanego statku (+1 bo nie liczylo wlacznie z pierwszym polem)
                 if x2 < x:
@@ -98,7 +98,7 @@ class ShipsContainer():
             print("Nie ma ustawiam")
             # Nastepnie stworzenie statku ,dodanie do listy i usuniecie z ships_to_set 1
             s = Ship(x, y, x2, y2)
-            print(s.get_miejsca()) #tmp
+
             #self.__ships_to_set.pop()
             self.__list_of_ships.append(s)
         else:
@@ -114,7 +114,18 @@ if __name__ == '__main__':
     print("Chcesz dodac statek:")
     x= int(input("1/0: "))
     while x:
-        us.add_ship2()
-        us.podglad_statkow()
+        print("Podaj wspolrzedne statku: ")
+        x = int(input("Podaj x: "))     #Wspolrzedne jednego konca sktaku
+        y = int(input("Podaj y: "))
+
+        _longer_ship=int(input("Czy twoj statek jest dluzszy? 1/0:"))
+        if _longer_ship:
+            print("Podaj wspolrzedne statku: ")
+            x2 = int(input("Podaj x: "))     #Wspolrzedne drugiego konca sktaku
+            y2 = int(input("Podaj y: "))
+            us.add_ship2(x,y,x2,y2)
+            us.podglad_statkow()
+        else:
+            us.add_ship(x,y)
         print("Chcesz dodac statek:")
         x = int(input("1/0: "))
--- Ships/UserInterface.py
+++ Ships/UserInterface.py
@@ -1,23 +1,46 @@
+import Functionality as f
+
+
 class InterfaceUser():
     def __init__(self):
         """
         Tworzy:
         1. Wygląd interface (wyswietla dwie plansze i przycinski)
-        przechowalnie statkow dla uzytkownika i dla komputera
-
+        przechowalnie statkow dla uzytkownika i dla komputera ???
         """
-    def Gra(self):
+        self.us = f.ShipsContainer("user")
+    #def Gra(self):
         """
 
         :return:
         """
     def Faza_rozmieszcanie(self):
         """
+        :return:
+        """
+        print("Chcesz dodac statek:")
+        x = int(input("1/0: "))
+        while x:
+            print("Podaj wspolrzedne statku: ")
+            x = int(input("Podaj x: "))  # Wspolrzedne jednego konca sktaku
+            y = int(input("Podaj y: "))
+
+            longer_ship = int(input("Czy twoj statek jest dluzszy? 1/0:"))
+            if longer_ship:
+                print("Podaj wspolrzedne statku: ")
+                x2 = int(input("Podaj x: "))  # Wspolrzedne drugiego konca sktaku
+                y2 = int(input("Podaj y: "))
+                self.us.add_ship2(x, y, x2, y2)
+                self.us.podglad_statkow()
+            else:
+                self.us.add_ship(x, y)
+            print("Chcesz dodac statek:")
+            x = int(input("1/0: "))
+   # def Faza_strzelanie(self):
+        """
 
         :return:
         """
-    def Faza_strzelanie(self):
-        """
-
-        :return:
-        """
+if __name__ == '__main__':
+    x= InterfaceUser()
+    x.Faza_rozmieszcanie()
