dd5d0b14c324f4c76bceb81aa646214fdabfd9bd
Agness <agix999r@gmail.com>
2020-05-11 14:34:23 +0200

    Structure of User Interface

---
+++ Ships/UserInterface.py
@@ -0,0 +1,23 @@
+class InterfaceUser():
+    def __init__(self):
+        """
+        Tworzy:
+        1. Wygląd interface (wyswietla dwie plansze i przycinski)
+        przechowalnie statkow dla uzytkownika i dla komputera
+
+        """
+    def Gra(self):
+        """
+
+        :return:
+        """
+    def Faza_rozmieszcanie(self):
+        """
+
+        :return:
+        """
+    def Faza_strzelanie(self):
+        """
+
+        :return:
+        """
